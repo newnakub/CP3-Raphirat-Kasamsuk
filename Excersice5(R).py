2cd1cac764aa3f6576e6aece02481fa5a22a57e1
newnakub <83075040+newnakub@users.noreply.github.com>
2021-04-29 16:38:09 +0700

    Add files via upload

---
+++ Excersice5(R).py
@@ -0,0 +1,14 @@
+'ตัวแปรข้อมูล'
+Number1 =  int(input("ตัวเลขตัวที่ 1 = "))
+Number2 =  int(input("ตัวเลขตัวที่ 2 = "))
+
+'ตัวแปรแสดงผลลัพท์'
+result1 =  Number1 + Number2
+result2 =  Number1 - Number2
+result3 =  Number1 * Number2
+result4 =  int(Number1 / Number2)
+
+print("คำตอบของผลบวกคือ  =" , result1)
+print("คำตอบของผลลบคือ  =" , result2)
+print("คำตอบของผลคูณคือ  =" , result3)
+print("คำตอบของผลหารคือ  =" , result4)
